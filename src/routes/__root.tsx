8cc7d8c9a93828c36610a8fdf4e70c501c72714d
Omar Zahid <omarzahid@outlook.com>
2025-11-06 14:18:10 +0800

    UI Shell styles

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,15 +2,35 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { Nav } from "../components/Nav";
 import "./root.css";
 import { Header } from "../components/Header";
+import { makeStyles, tokens } from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  shell: {
+    display: "flex",
+    gap: tokens.spacingHorizontalXL,
+    height: "100vh",
+  },
+  contnet: {
+    flex: "1",
+    display: "flex",
+    flexDirection: "column",
+    overflowY: "auto",
+  },
+});
 
 export const Route = createRootRoute({
-  component: () => (
-    <div className="shell">
+  component: () => <Shell />,
+});
+
+const Shell = () => {
+  const styles = useStyles();
+  return (
+    <div className={styles.shell}>
       <Nav />
-      <div className="content">
+      <div className={styles.contnet}>
         <Header />
         <Outlet />
       </div>
     </div>
-  ),
-});
+  );
+};
