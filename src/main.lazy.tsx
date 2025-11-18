c296885ebf480fde45035392b8c10918b1992427
Aisha Binti Norizan <Aisha-Binti.Norizan@endava.com>
2025-11-18 17:14:16 +0800

    feat: integrate branding into theme

--- src/main.lazy.tsx
+++ src/main.lazy.tsx
@@ -6,9 +6,41 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FluentProvider, webDarkTheme } from "@fluentui/react-components";
+import type { BrandVariants, Theme } from "@fluentui/react-components";
+import { createDarkTheme, createLightTheme } from "@fluentui/react-components";
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
+
+const endavabrand: BrandVariants = {
+  10: "#070200",
+  20: "#271007",
+  30: "#43160E",
+  40: "#591B12",
+  50: "#701F15",
+  60: "#882419",
+  70: "#9F291D",
+  80: "#B63022",
+  90: "#CD3829",
+  100: "#E34230",
+  110: "#F74F3A",
+  120: "#FF6950",
+  130: "#FF856D",
+  140: "#FF9E88",
+  150: "#FFB5A3",
+  160: "#FFCABE",
+};
+
+const lightTheme: Theme = {
+  ...createLightTheme(endavabrand),
+};
+
+const darkTheme: Theme = {
+  ...createDarkTheme(endavabrand),
+};
+
+darkTheme.colorBrandForeground1 = endavabrand[110];
+darkTheme.colorBrandForeground2 = endavabrand[120];
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -21,5 +53,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </QueryClientProvider>
       </OidcProvider>
     </FluentProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
