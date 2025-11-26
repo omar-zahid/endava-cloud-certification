8fc2c47d666bb256f3f14465d523fd5b01a18a7f
Aisha Binti Norizan <Aisha-Binti.Norizan@endava.com>
2025-11-26 14:52:26 +0800

    feat: add a temporary flag

--- src/routes/protected2.lazy.tsx
+++ src/routes/protected2.lazy.tsx
@@ -8,12 +8,17 @@ export const Route = createLazyFileRoute("/protected2")({
 function Page() {
   const { decodedIdToken } = useOidc({ assert: "user logged in" });
 
+  const showContent = false;
+
+  //TODO: check if we need to display anything
   return (
     <>
-      <h3>
-        Hello {decodedIdToken.name}, this is a lazy route where authentication
-        is enforced (in ./protected.tsx)
-      </h3>
+      {showContent ? (
+        <h3>
+          Hello {decodedIdToken.name}, this is a lazy route where authentication
+          is enforced (in ./protected.tsx)
+        </h3>
+      ) : null}
     </>
   );
 }
