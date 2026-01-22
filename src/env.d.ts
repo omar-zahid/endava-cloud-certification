a7429cda5bc88b0d9735cb746b8b84f3a43dafbf
Omar Zahid <omarzahid@outlook.com>
2026-01-22 16:26:51 +0800

    Added API URL

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_OIDC_ISSUER_URI: string;
   readonly VITE_OIDC_CLIENT_ID: string;
   readonly VITE_OIDC_SCOPE: string;
+  readonly VITE_API_URL: string;
 }
 
 interface ImportMeta {
